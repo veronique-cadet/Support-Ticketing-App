48e94a005aaba0531d957bb0f1c82a00efe3b913
“Veronique <cadetvero@gmail.com>
2024-09-18 10:54:10 -0500

    App setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="text-center text-blue-500">
+     <Form /> 
+    </div>
+  );
+}
+
+export default App;
